123ad900748cfa0590f839a750baf95a29ec2086
JervisA <155098J@mymail.nyp.edu.sg>
2016-11-14 15:27:21 +0800

    Initial commit

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,18 @@
+package demo;
+
+/**
+ * Created by Jervis on 14/11/2016.
+ */
+        import javax.ws.rs.GET;
+        import javax.ws.rs.Path;
+        import javax.ws.rs.Produces;
+        import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
---
+++ src/demo/Hi.java
@@ -0,0 +1,28 @@
+package demo;
+
+/**
+ * Created by Jervis on 14/11/2016.
+ */
+        import javax.ws.rs.*;
+        import javax.ws.rs.core.*;
+
+@Path("/hi")
+public class Hi {
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String getMessage() {
+        return "<hi>Hi World!</hi>";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPlainMessage() {
+        return "Hi World!";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getHTMLMessage() {
+        return "<b>Hi World!</b>";
+    }
+}
